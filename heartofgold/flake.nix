c0304a1030d3cf72ab6ce2ecfa812602eaea6d80
Loui <marxloui@protonmail.com>
2024-08-26 13:48:04 +0200

    configured cosmic flake.nix

--- heartofgold/flake.nix
+++ heartofgold/flake.nix
@@ -1,15 +1,28 @@
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # NOTE: change "unstable" to "24.05" if you are using NixOS 24.05
+
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, nixos-cosmic }: {
+    nixosConfigurations = {
+      # NOTE: change "host" to your system's hostname
+      heartofgold = nixpkgs.lib.nixosSystem {
+        modules = [
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
+          nixos-cosmic.nixosModules.default
+          ./configuration.nix
+        ];
+      };
+    };
   };
 }
